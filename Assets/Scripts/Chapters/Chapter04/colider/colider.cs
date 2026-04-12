1540a34c08c5038c07804bc089c098486fa8cca7
pluv-yue <pluv-yue@github.com>
2026-04-12 13:02:35 +0800

    与谁同坐轩

--- Assets/Scripts/Chapters/Chapter04/colider/colider.cs
+++ Assets/Scripts/Chapters/Chapter04/colider/colider.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class voice : MonoBehaviour
+public class colider : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
